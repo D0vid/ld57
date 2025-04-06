c393fbda9cff2b9ad0d9cb855cb6e6beea60c889
aeoncl <c.lopez.dev@protonmail.com>
2025-04-06 17:41:53 +0200

    blink fast

--- word/blinking_word.gd
+++ word/blinking_word.gd
@@ -2,6 +2,6 @@ extends BaseWord
 
 func _ready() -> void:
 	var tween = create_tween()
-	tween.tween_property(self, "modulate:a", 0, 1)
-	tween.tween_property(self, "modulate:a", 1, 1)
+	tween.tween_property(self, "modulate:a", 0, .2)
+	tween.tween_property(self, "modulate:a", 1, .2)
 	tween.set_loops()
