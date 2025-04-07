e6e1ade41de64c05cc2b7bc3c958a4d5fa169ecc
aeoncl <c.lopez.dev@protonmail.com>
2025-04-07 12:18:10 +0200

    floaty guy

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,13 @@ extends Area2D
 
 class_name Player
 
+func _ready() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "position:y", position.y - 10, 1)
+	tween.tween_property(self, "position:y", position.y + 10, 1)
+	tween.set_loops()
+	
+
 var health: int  = 100 : get = _get_health, set = _set_health
 var insight: int = 0 : get = _get_insight, set = _set_insight
 
